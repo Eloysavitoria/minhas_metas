3e0a54a0adc4bdf4504a2edd5bf0e85ecc8aefa0
Eloysavitoria <eloysa.bortoluzzi@escola.pr.gov.br>
2026-05-22 09:35:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoe[i].onclick = function(){
+    
+        for(let i=0;i <botoes.length;i++){
+            
+        }
+    }
+}
